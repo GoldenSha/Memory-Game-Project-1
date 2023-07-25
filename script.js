90d3685a6fd2bdfea42087d066e1d72fa1be9ab7
Shamonica <shamonica22@gmail.com>
2023-07-25 16:12:06 -0500

    created arrays and styling

--- script.js
+++ script.js
@@ -1,3 +1,32 @@
 //cards information
 
-const cards = []
+const cards = [
+    {
+        name: "longcurls",
+        img: "img/longcurls.jpg",
+    },
+    {
+        name: "afropuff.jpg",
+        img: "img/afropuff.jpg",
+    },
+    {
+        name: "bob",
+        img: "img/bob.jpg",
+    },
+    {
+        name: "fro",
+        img: "img/fro.jpg",
+    },
+    {
+        name: "locs",
+        img: "img/locs.jpg",
+    },
+    {
+        name: "shortcurly",
+        img: "img/shortcurly.jpg",
+    },
+    {
+        name: "softwaves",
+        img: "img/softwaves.jpg",
+    }
+]
